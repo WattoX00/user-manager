dda421efe625ee8875b170850db9e8c681de440c
wattox00 <korschzozinho@gmail.com>
2026-02-13 18:32:08 +0100

    added listing and basic functionalities

--- app/functions.py
+++ app/functions.py
@@ -1,44 +1,52 @@
 import subprocess
 
-def userAdd():
-    newUserName = input('New User Name: ').strip()
+class Functions():
 
-    cmd = ["sudo", "useradd"]
+    # add a user
 
-    # Home directory
-    homeQ = input(f'Do you want to add home directory [home/{newUserName}] (Y/n) ').lower()
-    if homeQ in ("y", "yes", ""):
-        cmd.append("-m")
+    def userAdd():
+        newUserName = input('New User Name: ').strip()
 
-    # Groups
-    groupQ = input(f'Do you want to add ({newUserName}) to a group (Y/n) ').lower()
-    if groupQ in ("y", "yes", ""):
-        listGroups()
-        groupName = input('Primary group first, then secondary groups separated by space: ').lower()
-        groups = groupName.split()
+        cmd = ["sudo", "useradd"]
 
-        if len(groups) >= 1:
-            cmd += ["-g", groups[0]]
+        # Home directory
+        homeQ = input(f'Do you want to add home directory [home/{newUserName}] (Y/n) ').lower()
+        if homeQ in ("y", "yes", ""):
+            cmd.append("-m")
 
-        if len(groups) > 1:
-            cmd += ["-G", ",".join(groups[1:])]
+        # Groups
+        groupQ = input(f'Do you want to add ({newUserName}) to a group (Y/n) ').lower()
+        if groupQ in ("y", "yes", ""):
+            listGroups()
+            groupName = input('Primary group first, then secondary groups separated by space: ').lower()
+            groups = groupName.split()
 
-    cmd.append(newUserName)
+            if len(groups) >= 1:
+                cmd += ["-g", groups[0]]
 
-    subprocess.run(cmd)
+            if len(groups) > 1:
+                cmd += ["-G", ",".join(groups[1:])]
 
-def listGroups():
-    subprocess.run([
-        "bash",
-        "-c",
-        "getent group | awk -F: '$3 >= 1000 || $1 ~ /^(sudo|wheel|docker)$/ {print $1}'"
-    ])
+        cmd.append(newUserName)
 
-# userAdd()
+        subprocess.run(cmd)
 
-def deleteUser():
-    name = str(input(''))
+    # remove user with home dir
 
-    cmd = ["sudo", "userdel", "-r", name]
-    subprocess.run(cmd)
-deleteUser()
+    def deleteUser():
+        name = str(input(''))
+
+        cmd = ["sudo", "userdel", "-r", name]
+        subprocess.run(cmd)
+
+    def listUsers():
+        subprocess.run(['bash', '-c', '''getent passwd | awk -F: '$3 >= 1000 {print $1}'
+'''])
+    def listGroups():
+        subprocess.run([
+            "bash",
+            "-c",
+            "getent group | awk -F: '$3 >= 1000 || $1 ~ /^(sudo|wheel|docker)$/ {print $1}'"
+        ])
+
+
--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
 from functions import Functions
+
+while True:
+    print("1. add user\n2. remove user\n3. List groups")
+    inp = str(input('> '))
+    if inp == '1':
+        Functions.userAdd()
+    elif inp == '2':
+        Functions.deleteUser()
+    elif inp == '3':
+        Functions.listGroups()
+    elif inp == '4':
+        Functions.listUsers()
+    else:
+        print("nothing is selected")
+        break
